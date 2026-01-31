4125b74a32208d77d40a3cdc14712b31e367a514
csaizr12 <carlos.saiz.rios@gmail.com>
2026-01-31 17:27:42 +0100

    change list for none

--- src/gffread.py
+++ src/gffread.py
@@ -16,7 +16,7 @@ def run_gffread(outbase, genome_assembly, annotation_target, path, kinds=[]):
         kinds = ["transcripts", "proteins"]
     if not outpath.exists():
             outpath.mkdir(parents=True)
-    last_result = []
+    last_result = None
     for kind in kinds:
             kind = kind.split("_")[0]
             outfile = outpath / "{}.fasta".format(kind)
@@ -24,16 +24,16 @@ def run_gffread(outbase, genome_assembly, annotation_target, path, kinds=[]):
             print(cmd_run)
             if outfile.is_file():
                 log_msg = "Gffread in {} mode already, done, skipping it".format(kind)
-                last_result.append({"outfile": outfile, "log_msg": log_msg, "returncode": 0,
-                            "cmd": cmd})
+                last_result = {"outfile": outfile, "log_msg": log_msg, "returncode": 0,
+                            "cmd": cmd}
             else:
                 results = run(cmd_run, shell=True, capture_output=True)
                 if results.returncode == 0:
                     log_msg = "Gffread in {} mode successfully done".format(kind)
                 else:
                     log_msg = "Gffread in {} mode error: {}".format(kind, results.stderr.decode())
-                    last_result.append({"outfile": outfile, "log_msg": log_msg, "returncode": results.returncode,
-                            "cmd": cmd})
+                    last_result = {"outfile": outfile, "log_msg": log_msg, "returncode": results.returncode,
+                            "cmd": cmd}
     return last_result
 
 
