7e7c787c076093031a38a72f1e5db4199474ab97
csaizr12 <carlos.saiz.rios@gmail.com>
2026-01-31 17:24:11 +0100

    fix error target sequences

--- src/gffread.py
+++ src/gffread.py
@@ -16,25 +16,25 @@ def run_gffread(outbase, genome_assembly, annotation_target, path, kinds=[]):
         kinds = ["transcripts", "proteins"]
     if not outpath.exists():
             outpath.mkdir(parents=True)
+    last_result = []
     for kind in kinds:
             kind = kind.split("_")[0]
-            print(kind)
             outfile = outpath / "{}.fasta".format(kind)
             cmd_run = cmd.format(gffread_modes[kind], outfile, genome_assembly, annotation_target)
             print(cmd_run)
             if outfile.is_file():
                 log_msg = "Gffread in {} mode already, done, skipping it".format(kind)
-                return {"outfile": outfile, "log_msg": log_msg, "returncode": 0,
-                            "cmd": cmd}
+                last_result.append({"outfile": outfile, "log_msg": log_msg, "returncode": 0,
+                            "cmd": cmd})
             else:
                 results = run(cmd_run, shell=True, capture_output=True)
                 if results.returncode == 0:
                     log_msg = "Gffread in {} mode successfully done".format(kind)
                 else:
                     log_msg = "Gffread in {} mode error: {}".format(kind, results.stderr.decode())
-                return {"outfile": outfile, "log_msg": log_msg, "returncode": results.returncode,
-                            "cmd": cmd}
-                    
+                    last_result.append({"outfile": outfile, "log_msg": log_msg, "returncode": results.returncode,
+                            "cmd": cmd})
+    return last_result
 
 
 
