a15fa73fdeafe7aed14ae0849ee2c503a2c7a910
csaizr12 <carlos.saiz.rios@gmail.com>
2026-02-05 19:26:38 +0100

    Update gffcompare.py

--- src/gffcompare.py
+++ src/gffcompare.py
@@ -1,4 +1,5 @@
 from subprocess import run
+from pathlib import Path
 
 def run_gffcompare(outbase, protein_path, transcripts_path, 
                    anotation_path, results, kinds=[]):
@@ -16,7 +17,7 @@ def run_gffcompare(outbase, protein_path, transcripts_path,
         else:
             continue
         outfile = (outpath/"{}.gffcompare".format(kind)).resolve()
-        cmd_run = cmd.format(anotation_path, outfile, evidence_file)
+        cmd_run = cmd.format(Path(anotation_path).resolve(), outfile, Path(evidence_file).resolve())
         print(cmd_run)
         if outfile.is_file():
              log_msg = "Gffcompare already done, skipping it"
