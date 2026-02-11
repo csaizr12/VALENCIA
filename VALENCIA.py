77d8e50aeaa990cc42d55028e54a473fbea723e9
csaizr12 <carlos.saiz.rios@gmail.com>
2026-02-11 09:58:04 +0100

    Update VALENCIA.py

--- VALENCIA.py
+++ VALENCIA.py
@@ -1,6 +1,7 @@
 # import 
 import argparse
 import os
+import sys
 
 from pathlib import Path
 
@@ -41,6 +42,7 @@ def get_arguments():
 # function main
 def main():
     print(">>> Working directory:", os.getcwd())
+    print("Command: {}".format(" ".join(sys.argv)))
 
     args = get_arguments()
     outbase = args["outbase"]
