4424592ed8bd9e7733b58bb0b3ac30e0993caed6
csaizr12 <carlos.saiz.rios@gmail.com>
2026-02-12 14:36:01 +0100

    Update dict_gene_iso_evidence.py

--- src/dict_gene_iso_evidence.py
+++ src/dict_gene_iso_evidence.py
@@ -1,7 +1,9 @@
+import os
 def add_refmap_info(gene_isoform_dict, refmap_path):
-    if 'trasncripts' in refmap_path:
+    fname = os.path.basename(refmap_path).lower()
+    if 'trasncripts' in fname:
         evidence_type = 'transcripts'
-    elif 'proteins' in refmap_path:
+    elif 'proteins' in fname:
         evidence_type = 'proteins'
     else:
         evidence_type = 'unknown'
