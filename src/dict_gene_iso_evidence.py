8a6b7e2cc042233f0419d9356ed911fa7a62ad60
csaizr12 <carlos.saiz.rios@gmail.com>
2026-02-12 14:10:32 +0100

    Update dict_gene_iso_evidence.py

--- src/dict_gene_iso_evidence.py
+++ src/dict_gene_iso_evidence.py
@@ -14,13 +14,13 @@ def add_refmap_info(gene_isoform_dict, refmap_path):
             class_code = fields[2]
             id_list = fields[3]
 
-            parts = id_list.split("|")
-            if len(parts) < 2:
-                 continue
-            gen_id = parts[0]
-            isoform_id = parts[1]
+            gen_id, isoform_list = id_list.split("|")
 
+            gen_id = gen_id.strip()
+            isoforms = [i.strip() for i in isoform_list.split(",") ]
             if gen_id in gene_isoform_dict:
+                    continue
+            for isoform_id in isoforms:
                     gene_isoform_dict[gen_id][isoform_id]['class_code'] = class_code
                     gene_isoform_dict[gen_id][isoform_id]['evidence_type'] = evidence_type
          
