d631cb4415595688e92c8dfa6f10af0001d4a209
csaizr12 <carlos.saiz.rios@gmail.com>
2026-02-12 13:29:14 +0100

    Create dict_gene_iso_evidence.py

---
+++ src/dict_gene_iso_evidence.py
@@ -0,0 +1,31 @@
+def add_refmap_info(gene_isoform_dict, refmap_path):
+    if 'trasncripts' in refmap_path:
+        evidence_type = 'transcripts'
+    elif 'proteins' in refmap_path:
+        evidence_type = 'proteins'
+
+    with open(refmap_path, "r") as f:
+        for line in f:
+            if line.startswith("ref_gene"):
+                continue
+            fields = line.strip().split('\t')
+            class_code = fields[2]
+            id_list = fields[3]
+
+            gen_id, isoform_id = id_list.split("|")
+
+            if gen_id in gene_isoform_dict:
+                    gene_isoform_dict[gen_id][isoform_id['class_code']] = class_code
+                    gene_isoform_dict[gen_id][isoform_id['evidence_type']] = evidence_type
+         
+    return gene_isoform_dict
+
+
+        
+
+
+
+
+
+        
+
