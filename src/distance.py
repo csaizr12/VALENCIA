2dac6b719874c9591b5ae407bae8d4b7205ed7fd
csaizr12 <carlos.saiz.rios@gmail.com>
2026-02-18 19:05:36 +0100

    Update distance.py

--- src/distance.py
+++ src/distance.py
@@ -16,16 +16,16 @@ def edit_distance(gene_isoform_dict, transcript_target_fasta, transcript_evidenc
                 continue
             #  3. Detectar si es transcript o protein 
             evidence_type = list(info.keys())[0]
-
             if evidence_type == "transcripts":
-                if iso_id not in records_transcript_target or iso_id not in records_transcript_evidence:
+                id = iso_id.split(".Araport11")[0]
+                if id not in records_transcript_target or id not in records_transcript_evidence:
                     print('not target:', iso_id)
                     continue
                  #  4. Cargar secuencias target/evidence 
                 seq_target = str(records_transcript_target[iso_id].seq)
                 seq_evidence = str(records_transcript_evidence[iso_id].seq)
             elif evidence_type == "protein":
-                 if iso_id not in records_protein_target or iso_id not in records_protein_evidence:
+                 if id not in records_protein_target or id not in records_protein_evidence:
                     print('not evidence:', iso_id)
                     continue
                 # 4. Cargar secuencias target/evidence
