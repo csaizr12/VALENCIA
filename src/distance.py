8dab7268075b244e2f58621ca93aa3c6f1685f4f
csaizr12 <carlos.saiz.rios@gmail.com>
2026-02-19 13:18:01 +0100

    distance + pseudocode

--- src/distance.py
+++ src/distance.py
@@ -3,38 +3,36 @@ from Levenshtein import distance
 
 def edit_distance(gene_isoform_dict, transcript_target_fasta, transcript_evidence_fasta,
              protein_target_fasta, protein_evidence_fasta):
-    #  1. Indexar los FASTA
+    # index fasta files
     records_transcript_target = SeqIO.index(transcript_target_fasta, "fasta")
     records_transcript_evidence = SeqIO.index(transcript_evidence_fasta, "fasta")
     records_protein_target = SeqIO.index(protein_target_fasta, "fasta")
     records_protein_evidence = SeqIO.index(protein_evidence_fasta, "fasta")
 
-    #  2. Recorrer gene_dict 
+    # run gene_isoform_dict  
     for gene_id, isoforms in gene_isoform_dict.items():
         for iso_id, info in isoforms.items():
             if not info:
                 continue
-            #  3. Detectar si es transcript o protein 
+            # obtein evidence type and mach_id
             evidence_type = list(info.keys())[0]
             match_id = info[evidence_type]['match_sequence']
-
+            # if iso_id and match_in in records, charge sequences
             if evidence_type == "transcripts":
                 if iso_id not in records_transcript_target or match_id not in records_transcript_evidence:
                     continue
-                 #  4. Cargar secuencias target/evidence 
                 seq_target = str(records_transcript_target[iso_id].seq)
                 seq_evidence = str(records_transcript_evidence[match_id].seq)
             elif evidence_type == "protein":
                  if iso_id not in records_protein_target or match_id not in records_protein_evidence:
                     continue
-                # 4. Cargar secuencias target/evidence
                  seq_target = str(records_protein_target[iso_id].seq)
                  seq_evidence = str(records_protein_evidence[match_id].seq)
             else:
                 continue
-            #  5. Calcular distancia 
+            # obtein edit distance with target and evidence
             edit_distance = distance(seq_target, seq_evidence)
-            # 6. Guardarla en gene_dic
+            # save it in a gene_isoform_dict
             gene_isoform_dict[gene_id][iso_id]["edit_distance"] = edit_distance
     print(gene_isoform_dict)
 
