3f4d2c07237d37e2bd450d82c02e64d2fc7c59cd
csaizr12 <carlos.saiz.rios@gmail.com>
2026-02-25 13:04:02 +0100

    Create add_features_to_gff.py

---
+++ src/add_features_to_gff.py
@@ -0,0 +1,7 @@
+# necesito: archivo de arabidopsis araport 11 (gff), diccionario gen iso ()
+def add_features_to_gff(outbase, gff_file, gene_isoform_dict):
+# abrimos el archivo arabidopsis Araport 11
+    # abrimos el arhivo arabiopsis Araport 11 para editar 
+        # lo dejamos como esta pero si estamos en la columna de RNAm 
+            # añdimos la informacion del dict gen iso
+    # quiero que me guardes el resultado en outbase con un nombre similarS
