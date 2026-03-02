73f5e9b1b5e6a0051713f9f856e99540dfed6048
csaizr12 <carlos.saiz.rios@gmail.com>
2026-03-02 11:43:53 +0100

    Update add_features_to_gff.py

--- src/add_features_to_gff.py
+++ src/add_features_to_gff.py
@@ -43,9 +43,10 @@ def add_features_to_gff(outbase, gff_file, gene_isoform_dict):
                         new_attributes = attributes + ";" + ";".join(evidence_info)
                     else:
                         new_attributes = attributes + ";evidence_info=NA"
+
+                    fields[8] = new_attributes
+                    gff_output.write("\t".join(fields) + "\n")
+               
                 else:
-                    new_attributes = attributes + ";evidence_info=NA"
-                    gff_output.write("\t".join(fields[:8]) + "\t" + new_attributes + "\n")  
-        else:
-            gff_output.write(line)
+                    gff_output.write(line)
 
