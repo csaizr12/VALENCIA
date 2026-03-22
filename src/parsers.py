a0830af91a3519c7d8dbfd14c16d5c7195298342
csaizr12 <carlos.saiz.rios@gmail.com>
2026-03-22 19:28:43 +0100

    fix

--- src/parsers.py
+++ src/parsers.py
@@ -36,8 +36,7 @@ def get_gene_isoform_dict_from_target_annotation(target_annotation):
     return gene_isoform_dict
 
 # define a function that updates a gene-isform dictionary
-# using a reference map file, and adds the class code 
-# and match sequence information (evidence) to the corresponding isoforms 
+# using a reference map file, and adds the class code and match sequence information (evidence) to the corresponding isoforms 
 def add_refmap_info(gene_isoform_dict, refmap_path):
     # get only the filename part from the full reference map path
     fname = os.path.basename(refmap_path)
@@ -53,20 +52,17 @@ def add_refmap_info(gene_isoform_dict, refmap_path):
             if line.startswith("ref_gene"):
                 continue
             fields = line.strip().split('\t')
-            # get the class code from the 3rd column and translate 
-            # it to a more descriptive term using the CLASS_CODE_TRANSLATION dictionary
+            # get the class code from the 3rd column and translate it to a more descriptive term using the CLASS_CODE_TRANSLATION dictionary
             class_code = CLASS_CODE_TRANSLATION[fields[2]]
             for gene in fields[3].split(","):
                 gene_id, iso_id = gene.split('|')
                 # look up the gene_id in the gene_isoform_dict; if not found, get None.
                 target_gene = gene_isoform_dict.get(gene_id.strip(), None)
-                # if the gene is not found in the gene_isoform_dict, we skip it;
-                #  otherwise, we look for the isoform and update its evidence information
+                # if the gene is not found in the gene_isoform_dict, we skip it; otherwise, we look for the isoform and update its evidence information
                 if target_gene is None: 
                     continue
                 if target_gene:
                     if iso_id in target_gene:
-                        target_gene[iso_id].update({evidence_type: {"class_code":class_code, 
-                                                                    "match_sequence": fields[1]}})
+                        target_gene[iso_id].update({evidence_type: {"class_code":class_code, "match_sequence": fields[1]}})
 
     return gene_isoform_dict
