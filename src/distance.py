43ace4b9f51bdcbd3b460d4fed6710f04a9f33da
csaizr12 <carlos.saiz.rios@gmail.com>
2026-03-25 12:23:02 +0100

    Update distance.py

--- src/distance.py
+++ src/distance.py
@@ -41,6 +41,7 @@ def edit_distance(parsed_evidence_gene_isoform_dict, transcript_target_fasta, tr
                 #Adapted aed calc from ingenannot
                 sensitivity = shared_bases /(shared_bases + (len_evidence - shared_bases))
                 specificity = shared_bases / (shared_bases + (len_target - shared_bases))
+                print(sensitivity, specificity)
                 lev_edit_distance = 1 - ((sensitivity + specificity) /2)
                 # save it in a gene_isoform_dict
                 parsed_evidence_gene_isoform_dict[target_gene_id][target_isoform_id][evidence_type]["edit_distance"] = lev_edit_distance
