3a418e79dda72b69deb5f5cefd55651fc3bf2d27
csaizr12 <carlos.saiz.rios@gmail.com>
2026-05-06 13:15:33 +0200

    Update gffcompare.py

--- src/gffcompare.py
+++ src/gffcompare.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 import shutil
 
 from subprocess import run
@@ -25,27 +24,34 @@ def run_gffcompare(outbase, protein_path, transcripts_path,
         else:
             continue            
         # construct output file names and a list of expected suffixes
-        outfile = "{}".format(kind)
-        cmd_run = cmd.format(evidence_path.absolute(), outfile, Path(anotation_path.absolute()))
+        outfile = outpath/"{}".format(kind)
+        out_prefix = "{}.{}.{}"
+        suffixes = ["tmap", "refmap"]
+        cmd_run = cmd.format(evidence_path, outfile, anotation_path)
         # if output already exists, skip execution
-        if (outpath / outfile).is_file():
+        if outfile.is_file():
              log_msg = "Gffcompare already done, skipping it"
-             results["compare_"+kind] = {"outfile": outpath / outfile, "log_msg": log_msg,
+             results["compare_"+kind] = {"outfile": outfile, "log_msg": log_msg,
                                "returncode": 0, "cmd": cmd_run}
         # otherwise, run the command via a systeam shell
         else:
-            cmd_results = run(cmd_run, shell=True, capture_output=True, cwd=outpath)
+            cmd_results = run(cmd_run, shell=True, capture_output=True)
             # on success
             if cmd_results.returncode == 0:
                 # log the completion
                 log_msg = "Gffcompare successfully done"
-                results["compare_"+kind]= {"outfile": outpath / outfile, "log_msg": log_msg,
+                results["compare_"+kind]= {"outfile": outfile, "log_msg": log_msg,
                                 "returncode": cmd_results.returncode,
                                 "cmd": cmd_run}
+                # file relocation
+                for suffix in suffixes:
+                     ref_fpath = evidence_path.parent / out_prefix.format(kind, anotation_path.name, suffix)
+                     new_fpath = outpath/ "{}.{}.{}".format(kind, anotation_path.name, suffix)
+                     shutil.move(ref_fpath, new_fpath)
             # on failure, capture and log the specific error details
             else:
                 log_msg = "Gffcompare error: {}".format(cmd_results.stderr.decode())
-                results["compare_"+kind]= {"outfile": outpath / outfile, "log_msg": log_msg, 
+                results["compare_"+kind]= {"outfile": outfile, "log_msg": log_msg, 
                                 "returncode": cmd_results.returncode,
                                 "cmd": cmd_run}
  
